21cf89dccb2fbedb1a6cb004ec51ff37c74e5c0e
wahyupurnama43 <wahyupurnama434@gmail.com>
2021-05-19 00:45:08 +0800

    updaate

--- src/js/ancor-link-smooth.js
+++ src/js/ancor-link-smooth.js
@@ -4,6 +4,7 @@ for (let index = 0; index < smoothScrollAnchor.length; index++) {
 
     el.addEventListener("click", function (ev) {
         ev.preventDefault();
+        // cek apakah ada attribut href yg memiliki tanda #
         if (document.getElementById(this.getAttribute("href").replace('#', ""))) {
             document.querySelector(this.getAttribute("href")).scrollIntoView({
                 behavior: "smooth",
